051451d08ce8bae2ccefe7e939cadb24e5e0702e
Emilymartin10 <54857971+Emilymartin10@users.noreply.github.com>
2019-11-18 15:31:30 -0700

    Login page / css

--- BattleBoats/Login.aspx
+++ BattleBoats/Login.aspx
@@ -5,12 +5,36 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
     <title>Login Page</title>
-
+    <link rel="stylesheet" type="text/css" href="CSS/Login.css" />
 </head>
 <body>
     <form id="form1" runat="server">
-        <div>
+        <div id="HeaderDiv">
+            <h2>Welcome to</h2>
+            <h1>BattleBoats!</h1>
         </div>
+        <section id="LoginDivs">
+            <div id="Registration">
+                <h3>New User</h3>
+                Username:
+                <br />
+                <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+                <br />
+                Password:
+                <br />
+                <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+                <br />
+                Confirm Password:
+                <br />
+                <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+                <br /><br />
+                <asp:Button ID="RegisterButton" runat="server" Text="Continue" />
+                <br /><br />
+            </div>
+            <div id="ExistingUser">
+
+            </div>
+        </section>
     </form>
 </body>
 </html>
--- BattleBoats/Login.aspx.designer.cs
+++ BattleBoats/Login.aspx.designer.cs
@@ -7,11 +7,13 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace BattleBoats {
-    
-    
-    public partial class Login {
-        
+namespace BattleBoats
+{
+
+
+    public partial class Login
+    {
+
         /// <summary>
         /// form1 control.
         /// </summary>
@@ -20,7 +22,7 @@ namespace BattleBoats {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
-        
+
         /// <summary>
         /// TextBox1 control.
         /// </summary>
@@ -29,5 +31,32 @@ namespace BattleBoats {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.TextBox TextBox1;
+
+        /// <summary>
+        /// TextBox2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox2;
+
+        /// <summary>
+        /// TextBox3 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox3;
+
+        /// <summary>
+        /// RegisterButton control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button RegisterButton;
     }
 }
