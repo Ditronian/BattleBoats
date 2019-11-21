e8cc4cd611a1aa7b7402fce0eeca8d22d1b02d51
Ditronian <davidgereau@gmail.com>
2019-11-21 10:40:14 -0700

    Made Score/Game classes

---
+++ BattleBoats/App_Code/Score.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BattleBoats.App_Code
+{
+    public class Score
+    {
+        private int ScoreID;
+        private int Hits;
+        private int Misses;
+        private int EnemyShipsSunk;
+
+        public int ScoreID1 { get => ScoreID; set => ScoreID = value; }
+        public int Hits1 { get => Hits; set => Hits = value; }
+        public int Misses1 { get => Misses; set => Misses = value; }
+        public int EnemyShipsSunk1 { get => EnemyShipsSunk; set => EnemyShipsSunk = value; }
+    }
+}
---
+++ BattleBoats/App_Code/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BattleBoats.App_Code
+{
+    public class User
+    {
+        private int UserID;
+        private string Username;
+        private Byte[] Password;
+        private int TotalGames;
+        private int GamesWon;
+        private int GamesLost;
+
+        public int UserID1 { get => UserID; set => UserID = value; }
+        public string Username1 { get => Username; set => Username = value; }
+        public byte[] Password1 { get => Password; set => Password = value; }
+        public int TotalGames1 { get => TotalGames; set => TotalGames = value; }
+        public int GamesWon1 { get => GamesWon; set => GamesWon = value; }
+        public int GamesLost1 { get => GamesLost; set => GamesLost = value; }
+    }
+}
