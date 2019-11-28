9d83d73bf5f427745fef81467c6268d5cf0dfef4
isaac <awesomeisaac2000@gmail.com>
2019-11-27 19:16:09 -0700

    Made and tested the now working union find data structure with path compression and weighted union.

--- BattleBoats/App_Code/Game/GameBoardUF.cs
+++ BattleBoats/App_Code/Game/GameBoardUF.cs
@@ -1,26 +1,29 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace BattleBoats
 {
-    // Beginings of union find data structure...
+    // Implements a union find data structure with path compression and weighted sensitive merging to acheive 
+    // best performance...
     public class GameBoardUF
     {
         private int[] elements;
-        private int[] size;
+        private int[] elemSize;
         private HashSet<int> components;
 
         public GameBoardUF(int size)
         {
             // Initialize arrays fill them with the correct values....
-            this.elements = new int[size];
-            this.size = new int[size];
+            elements = new int[size];
+            this.elemSize = new int[size];
+            components = new HashSet<int>();
 
             for (int i = 0; i < this.elements.Length; i++)
             {
-                this.elements[i] = i;
-                this.size[i] = 1;
-                this.components.Add(i);
+                elements[i] = i;
+                elemSize[i] = 1;
+                components.Add(i);
             }
         }
 
@@ -38,42 +41,48 @@ namespace BattleBoats
 
         public bool connected(int p, int q)
         {
+            // Cheack if the two elements have the same root...
             return root(p) == root(q);
         }
 
-        public void addEdge(int p, int q)
+        public void connect(int p, int q)
         {
             // If they are already connected just return....
             if (connected(p, q)) return;
             // Find the roots of p and q...
             int pRoot = root(p);
             int qRoot = root(q);
-            if (size[q] < size[p])
+            if (elemSize[q] < elemSize[p])
             {
                 // Case 1: Size of p tree is bigger, add q underneath p...
                 elements[qRoot] = pRoot;
-                size[pRoot] += size[qRoot];
-                size[qRoot] = 0;
+                elemSize[pRoot] += elemSize[qRoot];
+                elemSize[qRoot] = 0;
                 components.Remove(qRoot);
             }
             else
             {
                 // Case 2: Size of q was bigger, add p under q...
-                elements[qRoot] = pRoot;
-                size[qRoot] += size[pRoot];
-                size[pRoot] = 0;
+                elements[pRoot] = qRoot;
+                elemSize[qRoot] += elemSize[pRoot];
+                elemSize[pRoot] = 0;
                 components.Remove(pRoot);
             }
         }
 
         public int componentSize(int elem)
         {
-            return size[root(elem)];
+            return elemSize[root(elem)];
         }
 
-        public IEnumerator<int> getComponents()
+        public int[] getComponents()
         {
-            return this.components.GetEnumerator();
+            return this.components.ToArray();
+        }
+
+        public int numberComponents()
+        {
+            return components.Count;
         }
     }
 }
